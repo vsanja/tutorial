a39eca83a936fccfea063f778d592b3a53c3c157
Sanja Vujovic <sanja.vujovic@rbt.rs>
2016-08-11 16:00:29 +0200

    commit message

---
+++ 04 Ruby Bist 2/level_2/alias_method.rb
@@ -0,0 +1,16 @@
+# class Timeline
+#
+#   def initialize(tweets = [])
+#     @tweets = tweets
+#   end
+#
+#   def tweets
+#     @tweets
+#   end
+#
+#   def contents
+#     @tweets
+#   end
+# end
+
+#with ALIAS_METHOD
