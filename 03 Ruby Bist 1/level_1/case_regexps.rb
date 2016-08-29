e6db6b11cec2f5f128f15aad588fda5f8be3dd69
Sanja Vujovic <sanja.vujovic@rbt.rs>
2016-08-29 17:01:36 +0200

    message

--- 03 Ruby Bist 1/level_1/case_regexps.rb
+++ 03 Ruby Bist 1/level_1/case_regexps.rb
@@ -1,26 +1,10 @@
-# foo = "1Aheppsdf"
-#
-# what = case foo
-#          when /^[0-9]/
-#            "Begins with a number"
-#          when /^[a-zA-Z]/
-#            "Begins with a letter"
-#          else
-#            "Begins with something else"
-#        end
-# puts "String: #{what}"
+class Tweet
+  def initialize
+    self.tweet = tweet
+  end
+end
 
-
-# tweet_type = case tweet.status
-#                when /\A@\w+/
-#                  :mention
-#                when /\Ad\s+\w+/
-#                  :direct_message
-#                else
-#                  :public
-#              end
-
-tweet = "A@fwwwwwwff"
+tweet = Tweet.new("sjsjjs")
 
 tweet_type = case tweet.status
          when /\A@\w+/
